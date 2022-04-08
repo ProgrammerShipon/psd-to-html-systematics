f73507f69bcbd99990e59b5edcd557a3431df28d
Shipon Hossen Raju <78786587+ProgrammerShipon@users.noreply.github.com>
2022-04-08 20:53:43 +0600

    psd to html practice file

---
+++ js/index.js
@@ -0,0 +1,13 @@
+// Jquery Active start
+
+$(document).ready(function () {
+  // Bx Slider Active
+  $('.slider_area').bxSlider();
+  $('main_menu').meanmenu({
+    meanMenuContainer: '#mobile_menu',
+    meanMenuWidth: 768,
+  });
+});
+
+// Jquery Active end
+
